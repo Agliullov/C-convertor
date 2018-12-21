fa92dc66b84ccbe4ea452db90203f9f012656587
Agliullov <43278294+Agliullov@users.noreply.github.com>
2018-12-21 20:09:50 +0400

    Add files via upload

---
+++ ValueController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ValueController : ApiController
+    {
+        private readonly List<ValueRate> list;
+        public ValueController()
+        {
+            list = new List<ValueRate>
+            {
+                new ValueRate() {value = Value.Foot, sizeval = 1.0 },
+                new ValueRate() {value = Value.Metr, sizeval = 5.0 },
+                new ValueRate() {value = Value.Mile, sizeval = 10.0 },
+                new ValueRate() {value = Value.Yard, sizeval = 100.0 },
+            };
+
+        }
+
+        [HttpGet]
+        [Route("abc")]
+        public double CalcVal(Value from, Value to, double magnityde)
+        {
+            var cons = list.ToDictionary(x => x.value);
+            if (magnityde <= 0) return 0;
+            else return magnityde * (cons[to].sizeval / cons[from].sizeval);
+        }
+    }
+}
---
+++ ValueControllerTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication1.Controllers;
+using WebApplication1.Models;
+
+namespace UnitTestProject1.Controllers
+{
+    [TestClass]
+    class ValueControllerTest
+    {
+        [TestMethod]
+        public void CalcVal_magnitydezero_shouldbezero()
+        {
+            //arguments
+            ValueController Val = new ValueController();
+            Value qwe = Value.Foot;
+            Value wer = Value.Metr;
+            double vars = 0;
+            //call
+            double result = Val.CalcVal(qwe, wer, vars);
+            //Expect
+            Assert.AreEqual(0, result);
+        }
+        [TestMethod]
+        public void CalcVal_magnitydezero_shouldbenegative()
+        {
+            //arguments
+            ValueController Val = new ValueController();
+            Value qwe = Value.Foot;
+            Value wer = Value.Metr;
+            double vars = 0;
+            //call
+            double result = Val.CalcVal(qwe, wer, vars);
+            //Expect
+            Assert.AreEqual(-1, result);
+        }
+     }
+}
